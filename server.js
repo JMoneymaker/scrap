679f5235ddaf5436a9c37906901266a72ad15d54
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-06-27 13:30:55 -0700

    add jobs

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
 
+// const express = require('express');
 const app = require('./lib/app');
+const { UI } = require('bull-board');
 
 const PORT = process.env.PORT || 7890;
 
+app.use('/', UI);
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
