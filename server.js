d8f4929e1e581a531e3c44444f68b29192af7913
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-06-30 14:14:30 -0700

    add args

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
+require('./lib/jobs/queue');
 
 // const express = require('express');
 const app = require('./lib/app');
-const { UI } = require('bull-board');
 
 const PORT = process.env.PORT || 7890;
-
-app.use('/', UI);
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
