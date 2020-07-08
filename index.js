6c717750dc361f76f15159f0852c83f35fb2b58d
JMoneymaker <jasmin.moneymaker@gmail.com>
2020-07-07 18:00:21 -0700

    naming

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { idListQueue, profileQueue, parsedProfileQueue } = require('./lib/jobs/queue');
+const { idListQueue, rawProfileQueue, parsedProfileQueue } = require('./lib/jobs/queue');
 
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 Promise.all([
   idListQueue.empty(),
-  profileQueue.empty(),
+  rawProfileQueue.empty(),
   parsedProfileQueue.empty()
 ])
   .then(() => Promise.all(alphabet.map(letter => idListQueue.add({ letter }))))
@@ -12,9 +12,10 @@ Promise.all([
   .catch(error => console.log('Error adding jobs', error))
   .finally(() => Promise.all(([
     idListQueue.close(),
-    profileQueue.close(),
+    rawProfileQueue.close(),
     parsedProfileQueue.close()
   ])));
+
 
 
 
